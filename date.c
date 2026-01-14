b73bf88c543139a8a49e33a2fd6654bf55774a01
ThatGuyCodes <jamielkylelan@outlook.com>
2026-01-14 22:13:30 +0000

    Add license and header comments to date.c

--- date.c
+++ date.c
@@ -1,3 +1,17 @@
+/*
+ * betterdate - Real-time clock display
+ * Copyright (C) 2026 ThatGuyCodes605
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
